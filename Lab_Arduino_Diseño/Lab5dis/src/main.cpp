b307f05baa1a1da52466e506bce12c1f295280bd
gar19085 <gar19085@uvg.edu.gt>
2023-02-18 12:00:16 -0600

    Main v1.1

--- Lab_Arduino_Diseño/Lab5dis/src/main.cpp
+++ Lab_Arduino_Diseño/Lab5dis/src/main.cpp
@@ -3,6 +3,9 @@
 //Francisco Montufar, 19379
 #include <Arduino.h>
 
+#include "BOT.h"
+#include "LED.h"
+
 #define Bot1 10
 #define Bot2 11
 #define Bot3 12
@@ -16,65 +19,21 @@ int C4bit = 0;
 int LED_C1[4] = {LD1, LD2, LD3, LD4};
 int LED_C2[4] = {LD1, LD2, LD3, LD4};
 
-class Button {
-  private:
-    byte pin;
-    byte state;
-    byte lastReading;
-    unsigned long lastDebounceTime = 0;
-    unsigned long debounceDelay = 50;
-
-  public:
-    Button(byte pin) {
-      this->pin = pin;
-      lastReading = LOW;
-      init();
-    }
-    void init() {
-      pinMode(pin, INPUT);
-      update();
-    }
-    void update() {
-      // De una vez implementar un anti-rebote en la clase 
-      // para ya no tener que regresar a ver este código
-      byte newReading = digitalRead(pin);
-      if (newReading != lastReading) {
-        lastDebounceTime = millis();
-      }
-      if (millis() - lastDebounceTime > debounceDelay) {
-        state = newReading;
-      }
-      lastReading = newReading;
-    }
-    byte getState() {
-      update();
-      return state;
-    }
-
-    void setState(byte state){
-      state = state;
-    }
-
-    bool isPressed() {
-      return (getState() == HIGH);
-    }
-};
-
 Button Bo1(Bot1);
 Button Bo2(Bot2);
 Button Bo3(Bot3);
 
+Led L1(LD1);
 
 void setup() {
   // put your setup code here, to run once:
-    pinMode(LD1, OUTPUT);
-    pinMode(LD2, OUTPUT);
-    pinMode(LD3, OUTPUT);
-    pinMode(LD4, OUTPUT);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Bo1.isPressed()){
+    L1.on();
+  }else{
+    L1.off();
+  }
 }
