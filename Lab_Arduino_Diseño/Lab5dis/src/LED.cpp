b8c2896589af0ea87e236a89ddbcf732a915697a
Francisco <mon19379@uvg.edu.gt>
2023-02-22 12:18:58 -0600

    YA JALA

--- Lab_Arduino_Diseño/Lab5dis/src/LED.cpp
+++ Lab_Arduino_Diseño/Lab5dis/src/LED.cpp
@@ -8,73 +8,23 @@ cont::cont(uint8_t pines[], uint8_t cuenta){
   else{
     x = 4;
   }
-
-  for(int i = 0; i < x; i++){
-    pin[i] = pines[i];
-  }
-
+}
+void cont::init(){
+  DDRD = B00111100;
   
 }
 
-void cont::init(){
-  for(int i = 0; i < x; i++){
-    pinMode(pin[i], OUTPUT);
-  }
-}
-
 void cont::incremento(){
-
-  if(x == 4){
-    max = 15;
-
-  }
-
-  else{
-    max = (1 << x) - 1 ;
-  }
-
-  uint8_t m = 1;
-  uint8_t valor;
-
- for(int i = 0; i < x ; i++){
-    valor = (c & m) ? 1:0;
-    digitalWrite(pin[i], valor);
-    m = m << 1;
- }
-
-  if(c >= max){
+  c++;
+  PORTD = c;
+  if(c == 60){
     c = 0;
   }
-
-  else{
-    c++;
-  }
+  
 }
 
 void cont::decremento(){
-   if(x == 4){
-    min = 0;
-
-  }
-
-  else{
-    min = (1 << x) -1 ;
-  }
-
-  uint8_t m = 1;
-  uint8_t valor;
-
- for(int i = 0; i < x ; i++){
-    valor = (c & m) ? 1:0;
-    digitalWrite(pin[i], valor);
-    m = m << 1;
- }
-
-  if(c < min){
-    c = 0;
-  }
-
-  else{
-    c--;
-  }
+  c--;
+  PORTD = c;
+  
 }
