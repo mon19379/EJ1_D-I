abc35048d0a81f2edd735c41c891f0ccc4828971
gar19085 <gar19085@uvg.edu.gt>
2023-02-24 22:42:43 -0600

    main v2.1

--- Lab_Arduino_Diseño/Lab5dis/src/main.cpp
+++ Lab_Arduino_Diseño/Lab5dis/src/main.cpp
@@ -7,27 +7,27 @@ Francisco Montufar, 19379
 #include "BOT.h"
 #include "LED.h"
 
-#define Bot1 10
-#define Bot2 11
-#define Bot3 12
+#define Boton1 10
+#define Boton2 11
+#define Boton3 12
 
 int toggle_bot1 = 0;
 int toggle_bot2 = 0;
 int toggle_bot2 = 0;
 int Selector = 1;
 
-int Contador_1;
-int Contador_2;
+int Selector_Contador_1;
+int Selector_Contador_2;
 
-uint8_t Cont1;
-uint8_t Cont2;
+uint8_t Contador1;
+uint8_t Contador2;
 
-cont c1((uint8_t[]){6, 7, 8, 9}, Cont1);
-cont c2((uint8_t[]){6, 7, 8, 9}, Cont2);
+cont c1((uint8_t[]){6, 7, 8, 9}, Contador1);
+cont c2((uint8_t[]){6, 7, 8, 9}, Contador2);
 
-Button Bo1(Bot1);
-Button Bo2(Bot2);
-Button Bo3(Bot3);
+Button Bo1(Boton1);
+Button Bo2(Boton2);
+Button Bo3(Boton3);
 
 void setup() {
   // put your setup code here, to run once:
@@ -45,10 +45,10 @@ void loop() {
 
   else if(toggle_bot1 == 1 && !Bo1.isPressed()){
     toggle_bot1 = 0;
-    if(Contador_1 == 1){
+    if(Selector_Contador_1 == 1){
       c1.incremento();
       }
-    if (Contador_2 == 1){
+    if (Selector_Contador_2 == 1){
       c2.incremento();    
       } 
     }
@@ -59,10 +59,10 @@ void loop() {
 
   else if(toggle_bot2 == 1 && !Bo2.isPressed()){
     toggle_bot2 = 0;
-    if(Contador_1 == 1){
+    if(Selector_Contador_1 == 1){
       c1.decremento();
       }
-    if (Contador_2 == 1){
+    if (Selector_Contador_2 == 1){
       c2.decremento();    
       }
     }
@@ -75,12 +75,12 @@ void loop() {
     toggle_bot2 = 0;
   }
   if(Selector == 1){
-    Contador_1 = 1;
-    Contador_2 = 0;
+    Selector_Contador_1 = 1;
+    Selector_Contador_2 = 0;
   }
   if(Selector == 2){
-    Contador_2 = 1;
-    Contador_1 = 0;
+    Selector_Contador_2 = 1;
+    Selector_Contador_1 = 0;
   }
   if(Selector == 3){
     Selector = 1;
